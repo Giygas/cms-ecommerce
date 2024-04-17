aef9fca8683665a469edaa3907ccfdb44a4b31d9
Giygas <gustavorossich@gmail.com>
2024-04-17 22:13:55 +0000

    Fixed bad height in product grid

--- src/lib/slices/ProductList/index.svelte
+++ src/lib/slices/ProductList/index.svelte
@@ -17,7 +17,7 @@
 				</PrismicLink>
 			{/if}
 		</div>
-		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+		<div class="grid auto-rows-min grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 			{#each slice.items as product}
 				<article
 					class="flex flex-col gap-4 border bg-primary-content drop-shadow-sm rounded-lg overflow-hidden w-full bg-primary/10"
@@ -27,16 +27,16 @@
 						class="aspect-auto object-cover max-h-48 min-h-48  lg:max-h-72 lg:min-h-72 w-full"
 					/>
 					<div class="px-4">
-						<h3 class="text-2xl font-bold">
+						<h3 class="text-2xl font-bold min-h-16">
 							<PrismicRichText field={product.title} />
 						</h3>
 
-						<p class="text-right -mt-4 p-0 mb-4">
+						<p class="text-right p-0 mb-4">
 							<span class="badge badge-primary rounded-md">
 								{product.category}
 							</span>
 						</p>
-						<p class="max-h-28 overflow-y-scroll min-h-20 lg:min-h-28">
+						<p class="max-h-20 overflow-y-scroll min-h-20 lg:min-h-28">
 							<PrismicRichText field={product.description} />
 						</p>
 						<p class="my-2 text-right">
