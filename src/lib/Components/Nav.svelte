a41bd1df08edd98ecd507dd29475c1f907667cc6
Giygas <gustavorossich@gmail.com>
2024-04-23 07:54:28 +0000

    Added snipcart

--- src/lib/Components/Nav.svelte
+++ src/lib/Components/Nav.svelte
@@ -9,15 +9,28 @@
 <div class="btm-nav">
 	{#each links as link}
 		{@const active = $page.data.page.uid == link.link.uid}
-		<button
-			class="text-primary flex flex-col justify-center align-middle items-center self-center"
-			class:active
-		>
-			<PrismicLink field={link.link} class="w-full flex flex-col  items-center">
-				<PrismicImage field={link.icon} class="w-6 h-6 "></PrismicImage>
-				<!-- <img src={link.icon.url} class="w-6 h-6 items-center self-center" alt={link.icon.alt} /> -->
-				{link.label}
-			</PrismicLink>
-		</button>
+		{#if link.link.uid != 'cart'}
+			<button
+				class="text-primary flex flex-col justify-center align-middle items-center self-center"
+				class:active
+			>
+				<PrismicLink field={link.link} class="w-full flex flex-col  items-center">
+					<PrismicImage field={link.icon} class="w-6 h-6 "></PrismicImage>
+					<!-- <img src={link.icon.url} class="w-6 h-6 items-center self-center" alt={link.icon.alt} /> -->
+					{link.label}
+				</PrismicLink>
+			</button>
+		{:else}
+			<button
+				class="snipcart-checkout text-primary flex flex-col justify-center align-middle items-center self-center"
+				class:active
+			>
+				<PrismicLink class="w-full flex flex-col  items-center">
+					<PrismicImage field={link.icon} class="w-6 h-6 "></PrismicImage>
+					<!-- <img src={link.icon.url} class="w-6 h-6 items-center self-center" alt={link.icon.alt} /> -->
+					{link.label}
+				</PrismicLink>
+			</button>
+		{/if}
 	{/each}
 </div>
